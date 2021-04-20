3008ecc5677096c7160bb0bafc25cfbe055a6c1e
mBodashko <m_bodashko@gmail.com>
2021-04-20 20:30:42 +0300

    initial commit

---
+++ src/main/java/ru/template/SpringTemplate.java
@@ -0,0 +1,11 @@
+package ru.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplate {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplate.class, args);
+	}
+}
---
+++ src/main/resources/db/changelog/2020-04-20-initial.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset k_nizamiev:2020-04-20-initial
+--comment Тестовая таблица
+
+
+create table users (
+	id bigint primary key,
+	name varchar(32) not null,
+	gender varchar(10) not null,
+	deleted boolean not null,
+	date date not null
+);
+create sequence users_seq;
